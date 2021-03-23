b1d7e027586535724d948ada95ea3522c41b53ad
Ricardo Martins <martinsc.ricardo@gmail.com>
2021-03-23 09:58:33 -0300

    setup

---
+++ src/main/java/com/zup/ecommerce/EcommerceZupApplication.java
@@ -0,0 +1,13 @@
+package com.zup.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceZupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/ecommerce/EcommerceZupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
